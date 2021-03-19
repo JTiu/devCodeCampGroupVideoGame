d088ab5267b139f2e64ff8262e886eddf8c00325
stephon nelson <stephon.nelson81@gmail.com>
2021-03-19 10:23:30 -0500

    Routed API data to base HTML page, labeled columns the headers and footers

--- datatracker/video_game.py
+++ datatracker/video_game.py
@@ -1,6 +1,9 @@
 import requests, json
 from flask import Flask, jsonify, request, redirect, flash, render_template, url_for, Blueprint
 from types import SimpleNamespace
+
+from datatracker.Models.video_games import Video_Games
+
 bp = Blueprint('video_game', __name__, )
 
 
@@ -9,38 +12,60 @@ def index():
     # get info saved as a variable and pass variable into view
     message = "Welcome to The DataTracker"
     response = requests.get('https://api.dccresource.com/api/games')
-    json_data = '{"name": "Nintendogs", "platform": "DS"}'
-    game = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
-    print(game[0].name)
-    print(type(game))
-    return render_template('video_game/Index.html', message=message, response=response)
+    # print(response.json())#1. this should call json to the terminal step 2. comment out json object direct print#
+    content = response.json()#function call#
+    print(type(content))#option, also dir(content) this is a list, go to th right to see options#
+    print(dir(content))  # option, also dir(content)#
+    print(len(content))  # gets length of list 16598
+    print(content[0])  # prints first index [0] of list# or -1 prints last element
+    sales_count = dict()
+    for game in content:
+        game_year = game["year"]
+        print("year of the game ", game_year)
+        if (game_year is not None) and (game_year>=2013):
+            game_platform = game["platform"]
+            game_global = game["globalSales"]
+            if game_platform in sales_count:
+                sales_count[game_platform] = sales_count[game_platform] + game_global
+            else:
+                sales_count[game_platform] = game_global
+            print("sales count now is: ", sales_count)
+            print("\n\n\n")
+    items = sales_count.items()
+    print(items)
+    platforms = [i[0] for i in items]
+    sales = [i[1] for i in items]
+    print("platforms: ", platforms)
+    print("sales: ", sales)
+    return render_template('base.html', consoleNames = platforms, sales=sales)
+    #return render_template('video_game/Index.html', message=message, response=response)#
+
 
 
 @bp.route('/CopiesSoldPerConsole')
-def CopiesSoldPerConsole():
-    # use 3 lines from above to get games data in her
+def CopiesSoldPerConsole(json_data=None):
+    # use 3 lines from above to get games data in here
     #goal
-    # loop through entire data and build a new dictionary to represetn sales per console
-    # { 'ps3': 1000, 'ps4': 20000...}
+    # loop through entire data and build a new dictionary to represent sales per console
+    # { 'ps3': 1000, 'ps4': 20000...} 
     #steps
     # exclude entries prior to 2013
     # create a dictionary with key for each console in the data
     # loop through and add up total sales in appropraite key
-    message = "This is the chart for video game copies sold per console since 2013 "
-    response = requests.api('https://api.dccresource.com/api/games')
-    json_data = '{"date": "13", month : may }'
-    return render_template('video_game/index.html', game_data=message)
+
+    response = requests.get('https://api.dccresource.com/api/games', json_data)
+    game = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+    print(game[0].platform)
+    print(type(game))
+    return render_template('video_game/index.html', game_data=json_data, response=response)
+
+
 
 
 @bp.route('/test')
 def test():
     return "All good!"
 
-# @bp.route('/VideoGameBluePrint')
-# def index():
-#     message = "welcome to DataTracker"
-#     phrase = "Python is cool!"
-#     return render_template('/index.html', message=message, word=phrase)
 
 
 # @bp.route('/postform', methods=('GET', 'POST'))
